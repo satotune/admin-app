6dbe2276c6e4850db44626102be311a015aae43b
satotune <32587994+satotune@users.noreply.github.com>
2019-03-01 14:26:14 +0900

    ルーティングのエラーを修正

--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-
-  root to: RailsAdmin::Engine
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
